eb0ff741ee2a963e04fc2dd07c241aac04541327
joeljohnson2000 <peterjoel2000j@gmail.com>
2022-08-12 07:21:04 +0530

    table label filling optimization

--- timetracker/time_tracker/doctype/time_tracker/time_tracker.js
+++ timetracker/time_tracker/doctype/time_tracker/time_tracker.js
@@ -2,12 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Time Tracker', {
-	onload: function(frm) {
+	onload: function (frm) {
 		frm.clear_table("totals");
 		frm.add_child("totals");
 	},
-	project: function(frm) {
-		frm.set_query("task", "details", function(doc, cdt, cdn) {
+	project: function (frm) {
+		frm.set_query("task", "details", function (doc, cdt, cdn) {
 			let projects = []
 			let mates = []
 			for (let i = 0; i < frm.doc.project.length; i++) {
@@ -27,8 +27,8 @@ frappe.ui.form.on('Time Tracker', {
 			};
 		});
 	},
-	teammates: function(frm) {
-		frm.set_query("task", "details", function(doc, cdt, cdn) {
+	teammates: function (frm) {
+		frm.set_query("task", "details", function (doc, cdt, cdn) {
 			let projects = []
 			let mates = []
 			for (let i = 0; i < frm.doc.project.length; i++) {
@@ -48,59 +48,58 @@ frappe.ui.form.on('Time Tracker', {
 			};
 		});
 	},
-	from: function(frm) {
+	from: function (frm) {
+
+		let from_date = new Date(frm.doc.from);
+		let day_no = from_date.getDay();
+		if (day_no !== 1) {
+			frm.set_value("from", frappe.datetime.add_days(frm.doc.from, -1 * (day_no - 1)));
+		}
 		frm.set_value("to", frappe.datetime.add_days(frm.doc.from, 6));
 		frm.refresh_fields();
+		let fields_list = ["day_1", "day_2", "day_3", "day_4", "day_5", "day_6", "day_7"];
+		let week_days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-		var day_1 = frappe.meta.get_docfield("Time Tracker Detail","day_1", frm.doc.name);
-		day_1.label = "Mon (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 0)).split("-")[2].split(" ")[0] + ")";
-		var day_2 = frappe.meta.get_docfield("Time Tracker Detail","day_2", frm.doc.name);
-		day_2.label = "Tue (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 1)).split("-")[2].split(" ")[0] + ")";
-		var day_3 = frappe.meta.get_docfield("Time Tracker Detail","day_3", frm.doc.name);
-		day_3.label = "Wed (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 2)).split("-")[2].split(" ")[0] + ")";
-		var day_4 = frappe.meta.get_docfield("Time Tracker Detail","day_4", frm.doc.name);
-		day_4.label = "Thu (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 3)).split("-")[2].split(" ")[0] + ")";
-		var day_5 = frappe.meta.get_docfield("Time Tracker Detail","day_5", frm.doc.name);
-		day_5.label = "Fri (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 4)).split("-")[2].split(" ")[0] + ")";
-		var day_6 = frappe.meta.get_docfield("Time Tracker Detail","day_6", frm.doc.name);
-		day_6.label = "Sat (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 5)).split("-")[2].split(" ")[0] + ")";
-		var day_7 = frappe.meta.get_docfield("Time Tracker Detail","day_7", frm.doc.name);
-		day_7.label = "Sun (" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, 6)).split("-")[2].split(" ")[0] + ")";
-
+		for (let i = 0; i < fields_list.length; i++) {
+			var day = frappe.meta.get_docfield("Time Tracker Detail", fields_list[i], frm.doc.name);
+			let date = new Date(frappe.datetime.add_days(frm.doc.from, i));
+			let day_of_the_week = date.getDay();
+			day.label = week_days[day_of_the_week] + "(" + frappe.datetime.get_datetime_as_string(frappe.datetime.add_days(frm.doc.from, i)).split("-")[2].split(" ")[0] + ")";
+		}
 		frm.refresh_fields();
 	}
 });
 
 frappe.ui.form.on('Time Tracker Detail', {
-	day_2: function(frm, cdt, cdn) {
+	day_2: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	},
-	day_3: function(frm, cdt, cdn) {
+	day_3: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	},
-	day_4: function(frm, cdt, cdn) {
+	day_4: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	},
-	day_5: function(frm, cdt, cdn) {
+	day_5: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	},
-	day_6: function(frm, cdt, cdn) {
+	day_6: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	},
-	day_7: function(frm, cdt, cdn) {
+	day_7: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
 		row.total = row.day_2 + row.day_3 + row.day_4 + row.day_5 + row.day_6 + row.day_7;
 		frm.refresh_fields();
 	}
-	
+
 });
